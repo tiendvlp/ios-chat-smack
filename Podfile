df00dea787f5be1f0d92cab7a1bb060ef5e3905c
Đặng Tiến <tiendvlp@gmail.com>
2019-08-07 21:58:08 +0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '11.0'
+
+target 'Smack' do
+
+  use_frameworks!
+
+pod 'Alamofire'
+pod 'Socket.IO-Client-Swift'
+pod 'SwiftyJSON'
+
+end
---
+++ SmackTests/SmackTests.swift
@@ -0,0 +1,34 @@
+//
+//  SmackTests.swift
+//  SmackTests
+//
+//  Created by Đặng Tiến on 8/6/19.
+//  Copyright © 2019 Đặng Tiến. All rights reserved.
+//
+
+import XCTest
+@testable import Smack
+
+class SmackTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
